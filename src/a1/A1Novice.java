8287435641c9fc35678812d2e5d3cdad0a983079
abdik <abdik@DESKTOP-EBJF3KV.wireless-1x.unc.edu>
2019-09-01 19:50:30 -0400

    scanner initial setup and array for main outputs to print

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,32 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// Set up scanner to find first part which is amount of customers
+		// setting value of amtCustomer for first input
+		
+		int amtCustomer = scan.nextInt();
+		
+		// Now set up array to utilize the input of amount of customers...similar to A1Example
+		
+		int[] customerAmt = new int[amtCustomer];
+		
+		// set up array for other inputs (F+L names and total price of items purchased)
+		// First and Last names can use String and for total price would use double (real numbers)
+		// want these values since they are what is going to be printed as a result
+		
+		String[] firstName = new String[amtCustomer];
+		String[] lastName = new String[amtCustomer];
+		double[] amtTotal = new double[amtCustomer];
+		
+		
+				
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 }
