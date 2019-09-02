8117f6b6adeb8ba06a7543ba3093c0db8a7a2e06
abdik <abdik@DESKTOP-EBJF3KV.wireless-1x.unc.edu>
2019-09-01 22:46:16 -0400

    set up arrays and scanning component of item values

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -20,6 +20,7 @@ public class A1Novice {
 		// set up array for other inputs (F+L names and total price of items purchased)
 		// First and Last names can use String and for total price would use double (real numbers)
 		// want these values since they are what is going to be printed as a result
+		// Used Lect2 AvgHeight and example programs for reference
 		
 		String[] firstName = new String[amtCustomer];
 		String[] lastName = new String[amtCustomer];
@@ -27,14 +28,26 @@ public class A1Novice {
 		
 		// Setup the for loop (similar to A1Exp) in order to look for the values put in
 		// Remember setup of inputs... Number->String->String->Number->String->Number
+		// Recall Input process: # of customers->F+L names-># total items-> # of one item->item name->price
 		
 		for (int i=0; i<customerAmt.length; i++) {
 			firstName[i] = scan.next();
 			lastName[i] = scan.next();
 			
+			// set up scanner to find the number of total items utilizing the array made
 			
+		int numitemTotal = scan.nextInt();
 		
-				
+		// now want to make another array in order to save inputs related to the item.
+		// ex: # of total and singular items(integer), name of the item (string), price of the item (double)
+			
+		int[] numItem = new int[numitemTotal];
+		int[] singleItemAmt = new int[numitemTotal];
+		String[] itemName = new String[numitemTotal];
+		double[] itemPrice = new double[numitemTotal];
+		
+		// With arrays available, can now start the for loop to scan for respective values
+		
 		}
 		
 	}
