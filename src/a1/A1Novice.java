04e7cbb284c3700fb9f97d20518ce6e333e5170a
abdik <abdik@DESKTOP-EBJF3KV.wireless-1x.unc.edu>
2019-09-02 02:53:35 -0400

    made adjustment on period for printing

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -75,7 +75,7 @@ public class A1Novice {
 		// Use CharAt(0) to get the first letter of the string-> period, last name, then colon, total price.	
 		// Used Print part in A1Example as a reference
 			
-		System.out.println(firstName[i].charAt(0) + "." + lastName[i] + ": " + String.format("%.2f", amtTotalBeta));
+		System.out.println(firstName[i].charAt(0) + ". " + lastName[i] + ": " + String.format("%.2f", amtTotalBeta));
 				
 			}
 		
